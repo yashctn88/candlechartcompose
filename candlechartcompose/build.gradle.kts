72590257ceb53cc5dd474cda228711ffcb28478b
Yash Kalariya <yash.k@ixfi.com>
2024-01-29 18:04:16 +0530

    [Modified] Build file changes new

--- candlechartcompose/build.gradle.kts
+++ candlechartcompose/build.gradle.kts
@@ -18,6 +18,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
